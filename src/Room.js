9d2eae34bbf96b025327cad98cde28f52479c66d
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2017-01-22 00:17:37 +0800

    basic audio tracking

--- src/Room.js
+++ src/Room.js
@@ -4,9 +4,11 @@ import {
   Text,
   View,
   Dimensions,
+  DeviceEventEmitter,
 } from 'react-native';
 import Camera from 'react-native-camera';
 import RNFetchBlob from 'react-native-fetch-blob';
+import { AudioRecorder, AudioUtils } from 'react-native-audio';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -38,19 +40,90 @@ export default class Room extends Component {
 
     this.state = {
       captureImageInterval: null,
+      cntCaptureSound: 0,
+      cummulatedBelowBackground: 0,
+      background: 0,
+      audioPath: '/dev/null',
+      nextQuestion: false,
     };
 
     this.captureImage = this.captureImage.bind(this);
+    this.captureBackground = this.captureBackground.bind(this);
+    this.captureSound = this.captureSound.bind(this);
+    this.startTalk = this.startTalk.bind(this);
   }
 
-  componentWillMount() {
-      this.setState({
-        captureImageInterval: setInterval(this.captureImage, 5000)
-      })
+  componentDidMount() {
+      this.prepareRecordingPath(this.state.audioPath);
+      DeviceEventEmitter.addListener('recordingProgress', this.captureBackground);
+
+      AudioRecorder.startRecording();
+
+      setTimeout(this.startTalk, 10000);
   }
 
   componentWillUnmount() {
     clearInterval(this.state.captureImageInterval);
+    AudioRecorder.stopRecording();
+  }
+
+
+  prepareRecordingPath(audioPath){
+    AudioRecorder.prepareRecordingAtPath(audioPath, {
+      SampleRate: 22050,
+      Channels: 1,
+      AudioQuality: "Low",
+      AudioEncoding: "aac",
+      AudioEncodingBitRate: 32000,
+    });
+  }
+
+  captureBackground(data) {
+    console.log('bg', data, this.state.background);
+
+    const background = (data.currentTime !== 0) ? (this.state.background * this.state.cntCaptureSound + data.maxAmplitude) / data.currentTime : 0;
+
+    this.setState({
+      cntCaptureSound: data.currentTime,
+      background,
+    })
+  }
+
+  captureSound(data) {
+    console.log('progress', data, this.state.background);
+
+    let cummulatedBelowBackground = this.state.cummulatedBelowBackground;
+    let nextQuestion = this.state.nextQuestion;
+
+    if (!nextQuestion) { // Granny is not asking a question
+      if (data.maxAmplitude < this.state.background) {
+        cummulatedBelowBackground++;
+        if (cummulatedBelowBackground >= 3) {
+          nextQuestion = true;
+          cummulatedBelowBackground = 0;
+        }
+      } else {
+        cummulatedBelowBackground = 0;
+      }
+
+      this.setState({
+        cummulatedBelowBackground,
+        nextQuestion,
+      })
+
+      if (nextQuestion) this.nextQuestion();
+    }
+  }
+
+  nextQuestion() {
+    console.log('nextQuestion');
+
+    setTimeout(() => {
+      console.log('?');
+      this.setState({
+        nextQuestion: false,
+      });
+    }, 3000);
   }
 
   captureImage() {
@@ -61,11 +134,21 @@ export default class Room extends Component {
           'Content-Type' : 'application/octet-stream',
         }, data.data)
         .then((res) => {
-          console.log(res.text());
+          //console.log(res.text());
         })
         .catch((err) => console.log('Error: ', err));
       })
       .catch(err => console.error(err));
+  }
+
+  startTalk() {
+    console.log('startTalk');
+    DeviceEventEmitter.removeListener('recordingProgress', this.captureBackground);
+    DeviceEventEmitter.addListener('recordingProgress', this.captureSound);
+
+    this.setState({
+      captureImageInterval: setInterval(this.captureImage, 5000)
+    });
   }
 
   render() {
