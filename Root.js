986f7f18e64f717b0819e8d6ae9e4d53c16b48e4
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2017-01-22 09:42:06 +0800

    add router to report

--- Root.js
+++ Root.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Scene, Router } from 'react-native-router-flux';
 
 import Room from './src/Room'
+import Report from './src/Report'
 
 export default class Root extends Component {
 
   render() {
     return (
-      <Room />
+      <Router hideNavBar={true}>
+        <Scene key='room' component={Room} />
+        <Scene key='report' component={Report} />
+      </Router>
     );
   }
 }
