f298ce8958bc4708be74a5b1041ada4676272d17
gschua <gschua@connect.hku.hk>
2017-03-05 19:53:39 +0800

    include main menu

--- Root.js
+++ Root.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 
+import MainMenu from './src/MainMenu'
 import Room from './src/Room'
 import Report from './src/Report'
 
 export default class Root extends Component {
-
   render() {
     return (
       <Router hideNavBar={true}>
+        <Scene key='mainmenu' component={MainMenu} initial={true} />
         <Scene key='room' component={Room} />
         <Scene key='report' component={Report} />
       </Router>
