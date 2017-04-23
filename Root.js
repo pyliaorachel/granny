1411905767155552bdce1472380ef33c265dc247
gschua <gschua@connect.hku.hk>
2017-04-23 11:41:09 +0800

    Added database and member authentication

--- Root.js
+++ Root.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Scene, Router, ActionConst } from 'react-native-router-flux';
 
+import Account from './src/Account';
 import Main from './src/Main';
 import Room from './src/Room';
 import Report from './src/Report';
 import NavBar from './src/NavBar';
+import GrannyFirebase from "./src/database/GrannyFirebase";
 
 export default class Root extends Component {
-
+  constructor(props) {
+    super(props);
+    GrannyFirebase.initialise();
+  }
   render() {
     return (
       <Router>
+        <Scene key='account' component={Account} />
         <Scene key='main' component={Main} title='granny' />
         <Scene key='room' component={Room}  hideNavBar/>
         <Scene key='report' component={Report} title='Report' />
